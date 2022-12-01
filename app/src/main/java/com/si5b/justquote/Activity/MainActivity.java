e2a1d127d436c45a07baadaaea65394e8013e582
asda <matthewvincentius01@gmail.com>
2022-12-01 13:14:44 +0700

    Quote

---
+++ app/src/main/java/com/si5b/justquote/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.si5b.justquote.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si5b.justquote.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
